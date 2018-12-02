f5ba0a358396e72f65d029a2a8b465c47705046a
Miguel Gomes <jmbg.41@gmail.com>
2018-12-02 12:11:02 +0000

    Rename Package

--- src/main/java/com/example/graphql/GraphQlSpringBootApplication.java
+++ src/main/java/com/example/graphql/GraphQlSpringBootApplication.java
@@ -1,4 +1,4 @@
-package com.example.graphql.GraphQLSpringBoot;
+package com.example.graphql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
